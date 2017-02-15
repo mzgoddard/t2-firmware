ec835492322bdbe77802f90289abaf8b109a407a
Michael "Z" Goddard <mzgoddard@gmail.com>
2017-02-15 17:20:20 -0500

    Disable spid launching
    tesseldev is taking spid's place. As a first step, disable spid so it
    doesn't conflict with tesseldev in regards to socket communication with
    tessel apps.

--- soc/spid.sh
+++ soc/spid.sh
@@ -2,4 +2,4 @@
 mkdir -p /var/run/tessel
 ln -sf /var/run/tessel/1 /var/run/tessel/port_a
 ln -sf /var/run/tessel/2 /var/run/tessel/port_b
-exec spid /dev/spidev32766.1 2 1 /var/run/tessel
+# exec spid /dev/spidev32766.1 2 1 /var/run/tessel
