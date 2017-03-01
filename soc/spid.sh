a53af05a447b3ec0d1f342fea7bde82bd946fa7e
Michael "Z" Goddard <mzgoddard@gmail.com>
2017-03-01 17:19:59 -0500

    Link spi ports to tesselN in /var/run

--- soc/spid.sh
+++ soc/spid.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 mkdir -p /var/run/tessel
-ln -sf /var/run/tessel/1 /var/run/tessel/port_a
-ln -sf /var/run/tessel/2 /var/run/tessel/port_b
+ln -sf /var/run/tessel1 /var/run/tessel/port_a
+ln -sf /var/run/tessel2 /var/run/tessel/port_b
 # exec spid /dev/spidev32766.1 2 1 /var/run/tessel
